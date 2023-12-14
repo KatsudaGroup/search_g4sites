09ff575a4d8bebb06dbdc263aa4fb4f3311d58b3
Yuki Sakamoto <yuki.sakamoto@riken.jp>
2023-12-14 21:08:00 +0900

    modified regex to user variable length of the loop reagion

--- analyze_result.py
+++ analyze_result.py
@@ -1,10 +1,38 @@
 import pandas as pd
-df = pd.read_csv('search_result.tsv',  header = 0, sep = '\t')
+from matplotlib_venn import venn2
+import matplotlib.pyplot as plt
+import sys
+
+filename = sys.argv[1]
+#filename = 'search_result.tsv'
+df = pd.read_csv(filename,  header = 0, sep = '\t')
 
 nm_df = df[ df['type'] == 'NM' ]
 
+g22_series = nm_df['G2'] >= 2
+g31_series = (nm_df['G3'] >= 1) & (nm_df['G1'] >= 1)
+both_series = g22_series & g31_series
+g2nd_series = nm_df['G1'] >= 2
+len_g22 = len(nm_df[g22_series] )
+len_g31 = len(nm_df[g31_series] )
+len_both = len(nm_df[both_series])
+len_g2nd = len(nm_df[g2nd_series])
+
 print("All Entry: {}".format(len(df)))
 print("Entry starting with NM_: {}".format( len(nm_df) ))
-print("Entry of NM_, and G2 >= 2: {}".format( len(nm_df[nm_df['G2']>=2]) ) )
-print("Entry of NM_, and G3 >= 1 and G1 >= 1: {}".format(len( nm_df[ (nm_df['G3']>=1) & (nm_df['G1']>=1) ] )) )
-print( nm_df[ (nm_df['G3']>=1) & (nm_df['G1']>=1) ] ) 
+print("Entry of NM_, and G2 >= 2: {}".format(len(nm_df[g22_series])) )
+print("Entry of NM_, and G3 >= 1 and G1 >= 1: {}".format(len(nm_df[g31_series])) )
+print("Entry of NM_, and have Both motifs: {}".format(len(nm_df[g22_series & g31_series])) )
+print("Entry of NM_, and have either motifs: {}".format(len(nm_df[g22_series | g31_series])) )
+print("Entry of NM_, and have no motifs: {}".format(len(nm_df) - len(nm_df[g22_series | g31_series])) )
+print("Entry of NM_, and G1 >= 2: {}".format(len_g2nd) )
+
+plt.title('Human mRNAs (NM_*): {}'.format(len(nm_df)))
+n_g22_series = len(nm_df[g22_series])
+n_g31_series = len(nm_df[g31_series])
+n_both_series = len(nm_df[g22_series & g31_series]) 
+n_either_series = len(nm_df[g22_series | g31_series])
+venn2(
+    subsets = ([n_g22_series - n_both_series, n_g31_series - n_both_series, n_both_series] ), 
+    set_labels = ('2+2', '3+1')  )
+plt.show()
--- search_g4sites.py
+++ search_g4sites.py
@@ -18,7 +18,7 @@ def find_G4_2ndGeneration_pattern(sequence: str):
 def find_G4_22pattern(sequence: str):
     pos = 0
     array = []
-    g2_pattern = re.compile('GGG+[ATCGU]{1,7}GGG+')
+    g2_pattern = re.compile('GGG+.{1,7}GGG+')
 
     while True:
         m = g2_pattern.search(sequence, pos)
@@ -34,7 +34,7 @@ def find_G4_31pattern(sequence: str):
     array_3 = []
     array_1 = []
     array = []
-    g3_pattern = re.compile('GGG+[ATCGU]{1,7}GGG+[ATGCU]{1,7}GGG+')
+    g3_pattern = re.compile('GGG+.{1,7}GGG+.{1,7}GGG+')
     g1_pattern = re.compile('GGG+')
 
     pos = 0
@@ -56,19 +56,66 @@ def find_G4_31pattern(sequence: str):
                 pos = m.end()
     return (array_3, array_1)
 
-def process_file(fasta_file_path):
+def find_G4_22pattern_n(sequence: str, n: int):
+    pos = 0
+    array = []
+    g2_pattern = re.compile('GGG+.{1,' + str(n) + '}GGG+')
+
+    while True:
+        m = g2_pattern.search(sequence, pos)
+        if m is None:
+            break
+        else:
+            array.append(m.span())
+            pos = m.end()
+    return array
+
+def find_G4_31pattern_n(sequence: str, n: int):
+    pos = 0
+    array_3 = []
+    array_1 = []
+    array = []
+    g3_pattern = re.compile('GGG+.{1,' + str(n) + '}GGG+.{1,' + str(n) + '}GGG+')
+    g1_pattern = re.compile('GGG+')
+
+    pos = 0
+    while True:
+        m = g1_pattern.search(sequence, pos)
+        if m is None:
+            break
+        else:
+            # check the position if it is in G3 motif
+            g3_match = g3_pattern.match(sequence, m.start())
+            if g3_match is not None:
+                array_3.append(g3_match.span())
+                array.append(g3_match.span())
+                # skip
+                pos = g3_match.end()
+            else:
+                array_1.append(m.span())
+                array.append(m.span())
+                pos = m.end()
+    return (array_3, array_1)
+
+def process_file(fasta_file_path, n):
     from Bio import SeqIO
     for record in SeqIO.parse(fasta_file_path, 'fasta'):
         id_part = record.id
         desc_part = record.description
         str_seq = str(record.seq)
-        g2_pattern = find_G4_22pattern(str_seq)
-        (g3_pattern, g1_pattern) = find_G4_31pattern(str_seq)
+        g2_pattern = find_G4_22pattern_n(str_seq, n)
+        (g3_pattern, g1_pattern) = find_G4_31pattern_n(str_seq, n)
         print("{}\t{}\t{}\t{}\t{}\t{}".format(id_part, id_part[0:2], desc_part, len(g2_pattern), len(g3_pattern), len(g1_pattern)) )
 
 if __name__ == '__main__':
-    file_list = sys.argv
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', type = int, default = 7)
+    args, remain = parser.parse_known_args()
+    #file_list = sys.argv
+    file_list = remain
     print("{}\t{}\t{}\t{}\t{}\t{}".format("id", "type", "description", "G2", "G3", "G1") )
-    for filename in file_list[1:]:
-        process_file(filename)
+    for filename in file_list:
+        process_file(filename, args.n)
         #print(filename)
+
